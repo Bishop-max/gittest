d1d26c0436ed7f79d6097c176925c7ac6920c545
codermore <muguan1997@163.com>
2020-09-08 16:54:13 +0800

    correct configuration and add file-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 function component(){
 	var element = document.createElement('div');
 	
 	element.innerHTML = _.join(['hello','webpack'],' ');
 	element.classList.add('hello');
+	
+	//将图片导入现有的div
+	var myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
 	
 	return element;
 }
